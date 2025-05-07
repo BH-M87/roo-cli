b67d9c75bef44dbcbe5a9e576da889c6b2e2073f
Xuheng Liu <liuxuheng1990@gmail.com>
2025-05-08 00:45:34 +0800

    feat: enhance write_to_file tool description and parameters for clarity

--- src/core/prompts/tools/write-to-file.ts
+++ src/core/prompts/tools/write-to-file.ts
@@ -1,4 +1,4 @@
-import { ToolArgs } from './types';
+import { ToolArgs } from "./types";
 
 /**
  * 获取 write_to_file 工具的描述
@@ -7,10 +7,10 @@ import { ToolArgs } from './types';
  */
 export function getWriteToFileDescription(args: ToolArgs): string {
   return `## write_to_file
-Description: Write content to a file. If the file already exists, it will be overwritten.
+Description: Request to write full content to a file at the specified path. If the file exists, it will be overwritten with the provided content. If the file doesn't exist, it will be created. This tool will automatically create any directories needed to write the file.
 Parameters:
-- path: (required) The path to the file to write. This can be a relative path from the current working directory (${args.cwd}) or an absolute path.
-- content: (required) The content to write to the file.
+- path: (required) The path of the file to write to (relative to the current workspace directory ${args.cwd})
+- content: (required) The content to write to the file. ALWAYS provide the COMPLETE intended content of the file, without any truncation or omissions. You MUST include ALL parts of the file, even if they haven't been modified. Do NOT include the line numbers in the content though, just the actual content of the file.
 Usage:
 <write_to_file>
 <path>path/to/file.txt</path>
